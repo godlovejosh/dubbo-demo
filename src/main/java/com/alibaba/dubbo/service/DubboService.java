2a3994b3eddfb7ef2c9d2ee5bf58048d6e45ba37
wuxing <lfq@500mi.com>
2016-07-15 10:19:18 +0800

    archi

---
+++ src/main/java/com/alibaba/dubbo/service/DubboService.java
@@ -0,0 +1,9 @@
+package com.alibaba.dubbo.service;
+
+/**
+ * Created by wuxing on 16/7/13.
+ */
+public interface DubboService {
+
+    String getHelp();
+}
---
+++ src/main/java/com/alibaba/dubbo/service/DubboServiceImpl.java
@@ -0,0 +1,11 @@
+package com.alibaba.dubbo.service;
+
+/**
+ * Created by wuxing on 16/7/13.
+ */
+public class DubboServiceImpl implements DubboService {
+
+    public String getHelp() {
+        return "wait...";
+    }
+}
